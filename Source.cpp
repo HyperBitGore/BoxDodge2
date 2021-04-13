01f6a77e6da132e211ab9c618d775a0a7bfedf43
Joe <bruiserjoe01@gmail.com>
2021-04-13 11:25:37 -0500

    0.8
    Improved gameplay and started on pseudo lighting system

--- Source.cpp
+++ Source.cpp
@@ -11,15 +11,18 @@
 #undef main
 
 bool exitf = false;
-bool dead = false;
+bool dead = true;
 SDL_Renderer *rend;
 SDL_Window *window;
 SDL_Surface *surface;
+SDL_Texture* light;
 double delta = 0;
 Uint64 LAST = 0;
 int cbox = 0;
 int score = 0;
 int scale = 100;
+int speed = 200;
+int spawnc = 0;
 class Entity {
 public:
 	float x;
@@ -32,7 +35,7 @@ public:
 };
 std::vector<Entity> boxes;
 Entity player = {300, 300, 20, 20};
-SDL_mutex *mutex;
+Uint32 * lpixels = new Uint32[600*600];
 
 bool isColliding(Entity e, Entity e2) {
 	if (e.x < e2.x + e2.w && e.x + e.w > e2.x && e.y < e2.y + e2.h && e.y + e.h > e2.y) {
@@ -46,6 +49,8 @@ void death() {
 	player.x = 300;
 	boxes.clear();
 	scale = 100;
+	spawnc = 0;
+	speed = 200;
 }
 
 
@@ -70,12 +75,13 @@ int player_u(Entity p) {
 	return 0;
 }
 int box_u(Entity b) {
-	b.y += (float)200 * delta;
+	b.y += (float)speed * delta;
 	if (isColliding(b, player)) {
 		b.deathcall = true;
 	}
 	SDL_Rect rec = { b.x, b.y, b.w, b.h };
 	SDL_SetRenderDrawColor(rend, 255, 255, 255, 0);
+	
 	SDL_RenderFillRect(rend, &rec);
 	if (b.y > 601) {
 		b.erase = true;
@@ -91,16 +97,36 @@ void spawn_thread() {
 				Entity e;
 				e.x = rand() % 600;
 				e.y = 0;
-				e.w = rand() % 30 + 10;
-				e.h = rand() % 30 + 10;
+				e.w = rand() % 30 + 20;
+				e.h = rand() % 30 + 20;
 				e.erase = false;
 				e.Update = &box_u;
 				e.deathcall = false;
 				boxes.push_back(e);
+				spawnc++;
 			std::this_thread::sleep_for(std::chrono::milliseconds(scale));
 		}
 	}
 }
+//Write algorthim to handle caluclation of what alpha value should be
+//Also probably rewrite this to be more efficient 
+void updateLighting() {
+	int lpower = 255;
+	for (int i = 0; i < 600; i++) {
+		for (int j = 0; j < 600; j++) {
+			if (i == (int)player.y && j == (int)player.x) {
+				for (int l = i-10; l < i + player.h + 10; l++) {
+					for (int k = j-10; k < j + player.w + 10; k++) {
+						lpixels[l * 600 + k] = lpower;
+					}
+				}
+				
+			}
+		}
+	}
+
+}
+
 //https://wiki.libsdl.org/SDL_BlendMode?highlight=%28%5CbCategoryEnum%5Cb%29%7C%28SDLEnumTemplate%29
 int main(int argc, char **argv) {
 	srand(time(NULL));
@@ -120,16 +146,12 @@ int main(int argc, char **argv) {
 	Uint64 NOW = SDL_GetPerformanceCounter();
 	TTF_Font* font = TTF_OpenFont("MetalMania-Regular.ttf", 24);
 	TTF_Font* numfont = TTF_OpenFont("DelaGothicOne-Regular.ttf", 12);
-	mutex = SDL_CreateMutex();
-	std::thread box_manager(spawn_thread);
 	SDL_Event e;
 	SDL_Color white = { 255, 255, 255 };
 	SDL_Surface* text = TTF_RenderText_Solid(font, "Click to continue", white);
 	SDL_Texture* rtext = SDL_CreateTextureFromSurface(rend, text);
 	text = TTF_RenderText_Solid(font, "Your Score ", white);
 	SDL_Texture* rtext2 = SDL_CreateTextureFromSurface(rend, text);
-	SDL_Texture* screentex = SDL_CreateTextureFromSurface(rend, surface);
-	SDL_SetTextureBlendMode(screentex, SDL_BLENDMODE_BLEND);
 	SDL_Rect screenm;
 	screenm.x = 0;
 	screenm.y = 0;
@@ -141,6 +163,9 @@ int main(int argc, char **argv) {
 	msize.w = 600;
 	msize.h = 100;
 	float scalein = 0.0f;
+	light = SDL_CreateTexture(rend, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 600, 600);
+	memset(lpixels, 20, 600 * 600 * sizeof(Uint32));
+	std::thread box_manager(spawn_thread);
 	while (!exitf) {
 		LAST = NOW;
 		NOW = SDL_GetPerformanceCounter();
@@ -159,6 +184,17 @@ int main(int argc, char **argv) {
 		SDL_SetRenderDrawColor(rend, 0, 0, 0, 0);
 		SDL_RenderClear(rend);
 		if (!dead) {
+			updateLighting();
+			SDL_UpdateTexture(light, NULL, lpixels, 600 * sizeof(Uint32));
+			SDL_RenderCopy(rend, light, NULL, &screenm);
+			std::string out = std::to_string(score);
+			text = TTF_RenderText_Solid(numfont, out.c_str(), white);
+			SDL_Texture* rtext3 = SDL_CreateTextureFromSurface(rend, text);
+			msize.y = -10;
+			msize.w = 70;
+			SDL_RenderCopy(rend, rtext3, NULL, &msize);
+			SDL_DestroyTexture(rtext3);
+			SDL_FreeSurface(text);
 				player.Update(player);
 				for (auto& i : boxes) {
 					if (i.deathcall) {
@@ -177,12 +213,12 @@ int main(int argc, char **argv) {
 					}
 				}
 				cbox = 0;
-				SDL_RenderCopy(rend, screentex, NULL, &screenm);
 				scalein += delta;
 				if (scalein >= 2.0f) {
-					if (scale >= 51) {
+					if (scale >= 81) {
 						scale--;
-						std::cout << scale << std::endl;
+						speed+=10;
+						std::cout << speed << std::endl;
 					}
 					scalein = 0;
 				}
@@ -209,10 +245,10 @@ int main(int argc, char **argv) {
 		SDL_RenderPresent(rend);
 	}
 	box_manager.join();
+	delete[] lpixels;
 	TTF_Quit();
 	SDL_DestroyTexture(rtext);
 	SDL_DestroyTexture(rtext2);
-	SDL_DestroyMutex(mutex);
 	SDL_DestroyRenderer(rend);
 	SDL_FreeSurface(surface);
 	SDL_DestroyWindow(window);
