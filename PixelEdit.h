93ebf3d4305c554406f7f4ad9941b1dda73199e4
Joe <bruiserjoe01@gmail.com>
2021-04-18 14:03:32 -0500

    0.9
    Added basic lighting and fixed some shit

---
+++ PixelEdit.h
@@ -0,0 +1,81 @@
+#pragma once
+#include <iostream>
+#include <time.h>
+#include "SDL.h"
+
+
+namespace Gore {
+	struct RGB {
+		Uint8 g;
+		Uint8 b;
+		Uint8 r;
+		Uint8 a;
+	};
+	class Edit {
+	private:
+		Uint64 LAST = 0;
+		Uint64 NOW = SDL_GetPerformanceCounter();
+	public:
+		//Utility functions
+		double getDelta() {
+			double delta = 0;
+			LAST = NOW;
+			NOW = SDL_GetPerformanceCounter();
+			delta = (double)((NOW - LAST) * 1000 / (double)SDL_GetPerformanceFrequency());
+			delta = delta * 0.001;
+			return delta;
+		}
+		//Set the pixel color of a surface
+		//If you wanna use textures, just convert the surface to a texture
+		void setPixelRGB(SDL_Surface* tex, int x, int y, Uint8 r, Uint8 g, Uint8 b) {
+			SDL_LockSurface(tex);
+			Uint32* pixels = (Uint32*)tex->pixels;
+			Uint32* pixel = pixels + (y*tex->w) + x;
+			*pixel = SDL_MapRGB(tex->format, r, g, b);
+			SDL_UnlockSurface(tex);
+		}
+		//Copies raw pixel color data into a pixel
+		void setPixel(SDL_Surface* surf, int x, int y, Uint32 pixel) {
+			SDL_LockSurface(surf);
+			Uint32 *pixels = (Uint32*)surf->pixels;
+			pixels[(y*surf->w) + x] = pixel;
+			SDL_UnlockSurface(surf);
+		}
+		//Same as set pixel rgb but added alpha value
+		void setPixelRGBA(SDL_Surface* surf, int x, int y, Uint8 r, Uint8 g, Uint8 b, Uint8 a) {
+			SDL_LockSurface(surf);
+			Uint32* pixels = (Uint32*)surf->pixels;
+			Uint32* pixel = pixels + (y*surf->w) + x;
+			*pixel = SDL_MapRGBA(surf->format, r, g, b, a);
+			SDL_UnlockSurface(surf);
+		}
+		//returns the pixel color of a pixel at a certain x and y
+		Uint32 getPixel(SDL_Surface* surf, int x, int y) {
+			SDL_LockSurface(surf);
+			Uint32 *pixels = (Uint32 *)surf->pixels;
+			SDL_UnlockSurface(surf);
+			Uint32 pixel = pixels[(y * surf->w) + x];
+			return pixel;
+		}
+		//returns rgb value of pixel
+		RGB getPixelRGB(SDL_Surface* surf, int x, int y) {
+			SDL_LockSurface(surf);
+			Uint32 *pixels = (Uint32 *)surf->pixels;
+			Uint32 pixel = pixels[(y * surf->w) + x];
+			RGB c;
+			SDL_GetRGB(pixel, surf->format, &c.r, &c.g, &c.b);
+			SDL_UnlockSurface(surf);
+			return c;
+		}
+		//returns rgba value of pixel
+		RGB getPixelRGBA(SDL_Surface* surf, int x, int y) {
+			SDL_LockSurface(surf);
+			Uint32 *pixels = (Uint32 *)surf->pixels;
+			Uint32 pixel = pixels[(y * surf->w) + x];
+			RGB c;
+			SDL_GetRGBA(pixel, surf->format, &c.r, &c.g, &c.b, &c.a);
+			SDL_UnlockSurface(surf);
+			return c;
+		}
+	};
+}
--- Source.cpp
+++ Source.cpp
@@ -1,11 +1,9 @@
-#include <iostream>
+#include "PixelEdit.h"
 #include <windows.h>
 #include <vector>
 #include <thread>
-#include <time.h>
 #include <string>
 #include <chrono>
-#include "SDL.h"
 #include "SDL_ttf.h"
 #include "SDL_image.h"
 #undef main
@@ -15,7 +13,7 @@ bool dead = true;
 SDL_Renderer *rend;
 SDL_Window *window;
 SDL_Surface *surface;
-SDL_Texture* light;
+Gore::Edit edit;
 double delta = 0;
 Uint64 LAST = 0;
 int cbox = 0;
@@ -29,13 +27,12 @@ public:
 	float y;
 	int w;
 	int h;
-	int(*Update)(Entity e);
+	int(*Update)(Entity* e);
 	bool erase;
 	bool deathcall;
 };
 std::vector<Entity> boxes;
 Entity player = {300, 300, 20, 20};
-Uint32 * lpixels = new Uint32[600*600];
 
 bool isColliding(Entity e, Entity e2) {
 	if (e.x < e2.x + e2.w && e.x + e.w > e2.x && e.y < e2.y + e2.h && e.y + e.h > e2.y) {
@@ -56,7 +53,7 @@ void death() {
 
 
 
-int player_u(Entity p) {
+int player_u(Entity *p) {
 	if (GetAsyncKeyState(VkKeyScanA('w')) & 0x8000) {
 		player.y-= (float)500*delta;
 	}
@@ -68,26 +65,25 @@ int player_u(Entity p) {
 	}else if (GetAsyncKeyState(VkKeyScanA('a')) & 0x8000) {
 		player.x-= (float)500*delta;
 	}
-	SDL_Rect rec = { p.x, p.y, p.w, p.h};
+	SDL_Rect rec = { (*p).x, (*p).y, (*p).w, (*p).h};
 	SDL_SetRenderDrawColor(rend, 0, 0, 255, 0);
 	score++;
 	SDL_RenderFillRect(rend, &rec);
 	return 0;
 }
-int box_u(Entity b) {
-	b.y += (float)speed * delta;
-	if (isColliding(b, player)) {
-		b.deathcall = true;
+int box_u(Entity* b) {
+	(*b).y += (float)speed * delta;
+	if (isColliding((*b), player)) {
+		(*b).deathcall = true;
 	}
-	SDL_Rect rec = { b.x, b.y, b.w, b.h };
+	SDL_Rect rec = { (*b).x, (*b).y, (*b).w, (*b).h };
 	SDL_SetRenderDrawColor(rend, 255, 255, 255, 0);
 	
 	SDL_RenderFillRect(rend, &rec);
-	if (b.y > 601) {
-		b.erase = true;
+	if ((*b).y > 601) {
+		(*b).erase = true;
 	}
-	b.Update = &box_u;
-	boxes[cbox] = b;
+	(*b).Update = &box_u;
 	return 0;
 }
 
@@ -109,23 +105,24 @@ void spawn_thread() {
 	}
 }
 //Write algorthim to handle caluclation of what alpha value should be
-//Also probably rewrite this to be more efficient 
 void updateLighting() {
-	int lpower = 255;
-	for (int i = 0; i < 600; i++) {
-		for (int j = 0; j < 600; j++) {
-			if (i == (int)player.y && j == (int)player.x) {
-				for (int l = i-10; l < i + player.h + 10; l++) {
-					for (int k = j-10; k < j + player.w + 10; k++) {
-						lpixels[l * 600 + k] = lpower;
-					}
-				}
-				
-			}
+	SDL_LockSurface(surface);
+	int lpower = 10;
+	for (int l = player.y-10; l < player.y + player.h + 10; l++) {
+		for (int k = player.x-10; k < player.x + player.w + 10; k++) {
+			double distance = sqrt(pow((double)(k - (player.x+10)), 2.0) + pow((double)(l - (player.y+10)), 2.0));
+			lpower = 255 * (200 - (int)distance);
+			edit.setPixelRGBA(surface, k, l, 0, 0, 255, (Uint8)lpower);
 		}
 	}
-
+	SDL_UnlockSurface(surface);
 }
+void destroyLighting() {
+	SDL_LockSurface(surface);
+	std::memset(surface->pixels, 0, (600 * 600)*(sizeof(surface->pixels)));
+	SDL_UnlockSurface(surface);
+}
+
 
 //https://wiki.libsdl.org/SDL_BlendMode?highlight=%28%5CbCategoryEnum%5Cb%29%7C%28SDLEnumTemplate%29
 int main(int argc, char **argv) {
@@ -141,7 +138,17 @@ int main(int argc, char **argv) {
 	}
 	window = SDL_CreateWindow("Box Dodge 2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 600, SDL_WINDOW_SHOWN);
 	rend = SDL_CreateRenderer(window, -1, 0);
-	surface = SDL_GetWindowSurface(window);
+	surface = SDL_CreateRGBSurfaceWithFormat(0, 600, 600, 32, SDL_PIXELFORMAT_RGBA8888);
+	//SDL_SetSurfaceBlendMode(surface, SDL_BLENDMODE_BLEND);
+	edit.setPixelRGBA(surface, 1, 1, 255, 0, 0, 255);
+	edit.setPixelRGBA(surface, 2, 1, 255, 0, 0, 255);
+	edit.setPixelRGBA(surface, 3, 1, 255, 0, 0, 255);
+	edit.setPixelRGBA(surface, 1, 2, 255, 0, 0, 200);
+	edit.setPixelRGBA(surface, 2, 2, 255, 0, 0, 200);
+	edit.setPixelRGBA(surface, 3, 2, 255, 0, 0, 200);
+	edit.setPixelRGBA(surface, 1, 3, 255, 0, 0, 170);
+	edit.setPixelRGBA(surface, 2, 3, 255, 0, 0, 170);
+	edit.setPixelRGBA(surface, 3, 3, 255, 0, 0, 170);
 	player.Update = &player_u;
 	Uint64 NOW = SDL_GetPerformanceCounter();
 	TTF_Font* font = TTF_OpenFont("MetalMania-Regular.ttf", 24);
@@ -163,8 +170,6 @@ int main(int argc, char **argv) {
 	msize.w = 600;
 	msize.h = 100;
 	float scalein = 0.0f;
-	light = SDL_CreateTexture(rend, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 600, 600);
-	memset(lpixels, 20, 600 * 600 * sizeof(Uint32));
 	std::thread box_manager(spawn_thread);
 	while (!exitf) {
 		LAST = NOW;
@@ -185,8 +190,9 @@ int main(int argc, char **argv) {
 		SDL_RenderClear(rend);
 		if (!dead) {
 			updateLighting();
-			SDL_UpdateTexture(light, NULL, lpixels, 600 * sizeof(Uint32));
+			SDL_Texture* light = SDL_CreateTextureFromSurface(rend, surface);
 			SDL_RenderCopy(rend, light, NULL, &screenm);
+			SDL_DestroyTexture(light);
 			std::string out = std::to_string(score);
 			text = TTF_RenderText_Solid(numfont, out.c_str(), white);
 			SDL_Texture* rtext3 = SDL_CreateTextureFromSurface(rend, text);
@@ -195,22 +201,20 @@ int main(int argc, char **argv) {
 			SDL_RenderCopy(rend, rtext3, NULL, &msize);
 			SDL_DestroyTexture(rtext3);
 			SDL_FreeSurface(text);
-				player.Update(player);
+				player.Update(&player);
 				for (auto& i : boxes) {
 					if (i.deathcall) {
 						death();
 						break;
 					}
-					if (!i.Update(i)) {
-						cbox++;
-					}
-					else {
+					if (i.Update(&i)) {
 						i.erase = true;
 					}
 					if (i.erase) {
 						boxes.erase(boxes.begin() + cbox);
 						cbox--;
 					}
+					cbox++;
 				}
 				cbox = 0;
 				scalein += delta;
@@ -222,6 +226,7 @@ int main(int argc, char **argv) {
 					}
 					scalein = 0;
 				}
+				destroyLighting();
 		}
 		else {
 			std::string out = std::to_string(score);
@@ -245,7 +250,6 @@ int main(int argc, char **argv) {
 		SDL_RenderPresent(rend);
 	}
 	box_manager.join();
-	delete[] lpixels;
 	TTF_Quit();
 	SDL_DestroyTexture(rtext);
 	SDL_DestroyTexture(rtext2);
